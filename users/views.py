6c980cc6ba67226fa5b5a22b45209694d6e21ac4
YorkYu <ysmintor@gmail.com>
2017-01-17 15:33:53 +0800

    linking to the logout page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
